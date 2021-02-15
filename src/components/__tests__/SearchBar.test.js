da39983ff462df9d93e69c5625fb8b56003ba9e7
conrad <yhg0337@naver.com>
2021-02-16 01:35:51 +0900

    Add search-bar-testing

---
+++ src/components/__tests__/SearchBar.test.js
@@ -0,0 +1,34 @@
+import { shallowMount, createLocalVue, mount } from '@vue/test-utils';
+import Vuetify from 'vuetify';
+// import Vuex from 'vuex';
+import store from '@/store';
+import SearchBar from '../SearchBar';
+
+const localVue = createLocalVue();
+localVue.use(Vuetify);
+// 가짜 스토어를 연결할 경우
+// localVue.use(Vuex)
+
+// const store = new Vuex({
+//     modules:{
+
+//     }
+// })
+describe('SearchBar Component', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(SearchBar, { localVue, store });
+  });
+  test('제목을 입력하면 스토어가 업데이트', () => {
+    wrapper.vm.title = 'lion';
+    expect(wrapper.vm.title).toBe('lion');
+  });
+
+  test('로딩중 아이콘 확인', async () => {
+    wrapper.vm.$store.commit('movie/updateState', {
+      loading: true,
+    });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.find('.v-progress-circular').exists()).toBe(true);
+  });
+});
